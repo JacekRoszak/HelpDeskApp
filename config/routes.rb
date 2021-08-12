c585dfa95ffdc823c96066ef9bb07a0201b5e0b5
JacekRoszak <65806090+JacekRoszak@users.noreply.github.com>
2021-08-12 20:20:51 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,6 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   devise_for :users
-  devise_scope :user do
-    authenticated :user do
-      root 'pages#home', as: :authenticated_root
-    end
-  
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
+  root 'pages#home'
+
 end
