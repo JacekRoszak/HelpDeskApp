10883fefd365d5bddaa66396f8307f3b59d5be7e
JacekRoszak <65806090+JacekRoszak@users.noreply.github.com>
2021-08-17 20:08:34 +0200

    badgelabelhelper problem fix...

--- config/initializers/twitter_bootstrap_rails.rb
+++ config/initializers/twitter_bootstrap_rails.rb
@@ -1,2 +1,2 @@
-gem_dir = Gem::Specification.find_by_name("twitter-bootstrap-rails").gem_dir
-require "#{gem_dir}/app/helpers/badge_label_helper"
+# gem_dir = Gem::Specification.find_by_name("twitter-bootstrap-rails").gem_dir
+# require "#{gem_dir}/app/helpers/badge_label_helper"
